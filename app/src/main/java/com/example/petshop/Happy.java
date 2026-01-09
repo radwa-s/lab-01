a0b94ac5e36db0e4295615c99aa49c1ac613b9e6
rsheikhdon <rsheikhdon124@gmail.com>
2026-01-09 10:08:04 -0700

    Initial commit

---
+++ app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,14 @@
+package com.example.petshop;
+import java.util.Date;
+public class Happy extends Mood{
+    public Happy(){
+        super();
+    }
+    public Happy(Date date){
+        super(date);
+    }
+    @Override
+    public String getMood() {
+        return "Happy";
+    }
+}
